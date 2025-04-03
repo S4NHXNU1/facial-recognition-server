8be7bc633fdd5cd87f427623fd6c0038b3226437
S4NHXNU1 <sanhanut2003@gmail.com>
2025-04-03 15:31:19 +0700

    feat: adjust route responses

--- controller/authenController.py
+++ controller/authenController.py
@@ -2,6 +2,10 @@ from flask import Blueprint, jsonify
 
 auth = Blueprint("auth", __name__, url_prefix="/auth")
 
-@auth.route("/", methods=["GET"])
-def index():
-    return jsonify({"message": "this is auth route"}), 200
+@auth.route("/login", methods=["POST"])
+def login():
+    return jsonify({"message": "successfully logged in"}), 200
+
+@auth.route("/register", methods=["POST"])
+def register():
+    return jsonify({"message": "successfully registered"}), 201
--- controller/faceScanController.py
+++ controller/faceScanController.py
@@ -2,6 +2,10 @@ from flask import Blueprint, jsonify
 
 scan = Blueprint("scan", __name__, url_prefix="/scan")
 
-@scan.route("/", methods=["GET"])
-def index():
-    return jsonify({"message": "this is scan route"}), 200
+@scan.route("/register", methods=["POST"])
+def register():
+    return jsonify({"message": "successfully registered face data"}), 200
+
+@scan.route("/recognize", methods=["POST"])
+def recognize():
+    return jsonify({"message": "matched"}), 200
